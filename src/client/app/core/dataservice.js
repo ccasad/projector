d875ed6b424826023341ba0b5a05d9671605ec43
Chris Casad <ccasad@gmail.com>
2015-05-21 14:23:01 -0400

    commented out test code

--- src/client/app/core/dataservice.js
+++ src/client/app/core/dataservice.js
@@ -11,29 +11,29 @@
   function dataservice($http, $q, logger, restapi) {
     var service = {
       getProjects: getProjects,
-      getPeople: getPeople,
-      getMessageCount: getMessageCount
+      // getPeople: getPeople,
+      // getMessageCount: getMessageCount
     };
 
     return service;
 
-    function getMessageCount() { return $q.when(72); }
+    // function getMessageCount() { return $q.when(72); }
 
-    function getPeople() {
-      return $http.get('/api/people')
-        .then(success)
-        .catch(fail);
+    // function getPeople() {
+    //   return $http.get('/api/people')
+    //     .then(success)
+    //     .catch(fail);
 
-      function success(response) {
-        return response.data;
-      }
+    //   function success(response) {
+    //     return response.data;
+    //   }
 
-      function fail(error) {
-        var msg = 'query for people failed. ' + error.data.description;
-        logger.error(msg);
-        return $q.reject(msg);
-      }
-    }
+    //   function fail(error) {
+    //     var msg = 'query for people failed. ' + error.data.description;
+    //     logger.error(msg);
+    //     return $q.reject(msg);
+    //   }
+    // }
 
     function getProjects() {
       return $http.get(restapi.server+'api/projects')
